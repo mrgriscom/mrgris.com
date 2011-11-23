c366ee267b400e39d41c863784f73371e756887f
Drew Roos <droos@dimagi.com>
2011-11-23 02:07:00 -0500

    refactor fab support code

---
+++ fabutil/util.py
@@ -0,0 +1,25 @@
+from fabric.api import env
+from fabric.context_managers import lcd
+import os.path
+import os
+from contextlib import contextmanager, nested
+
+def rootdir():
+    return os.path.dirname(env.real_fabfile)
+
+def abspath(*paths):
+    return os.path.join(rootdir(), *paths)
+
+@contextmanager
+def chdir(dir):
+    cwd = os.getcwd()
+    os.chdir(dir)
+    try:
+        yield
+    finally:
+        os.chdir(cwd)
+
+def _cd(dir=None):
+    dir = dir or rootdir()
+    return nested(chdir(dir), lcd(dir))
+
