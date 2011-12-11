989761ba97aa704e723baefbfd458eab91f94aef
Drew Roos <droos@dimagi.com>
2011-12-11 14:36:12 -0500

    labrador tweaks

--- content/script/util.js
+++ content/script/util.js
@@ -2,12 +2,13 @@
 
 function scroll_pin(pin_threshold, $container, $elem) {
   var base_offset = $container.offset().top;
+  var pos_type = $elem.css('position');
 
   return function() {
     var scroll_pos = $(window).scrollTop();
     var elem_pos = base_offset - scroll_pos;
     var pinned = (elem_pos < pin_threshold);
-    $elem.css('position', pinned ? 'fixed' : 'static');
+    $elem.css('position', pinned ? 'fixed' : pos_type);
     $elem.css('top', pinned ? pin_threshold + 'px' : 0);
   };
 }
@@ -16,4 +17,10 @@ function set_pin(pin_threshold, $container, $elem) {
   var pinfunc = scroll_pin(pin_threshold, $container, $elem);
   $(window).scroll(pinfunc);
   pinfunc();
+}
+
+function preload_images(paths, basepath) {
+  $(paths).each(function(i, val) {
+    $('<img />').attr('src', (basepath || '') + val).prependTo('body').hide();
+  });
 }
