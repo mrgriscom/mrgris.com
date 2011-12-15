1b0c9b72f1b60269a0ccfc1f28043e16dedd1ca9
Drew Roos <droos@dimagi.com>
2011-12-15 10:59:20 -0500

    change where location precision is set

--- content/script/pos.php
+++ content/script/pos.php
@@ -1,2 +1,13 @@
 <? header('Content-Type: text/json'); ?>
-<?= file_get_contents('/var/www/a/curloc.json') ?>
+<?
+
+$PREC = 1;
+
+$raw = file_get_contents('/var/lib/webapp/drewtracker.json');
+$pos = json_decode($raw, true);
+
+$pos['lat'] = round($pos['lat'], $PREC);
+$pos['lon'] = round($pos['lon'], $PREC);
+
+?>
+<?= json_encode($pos); ?>
