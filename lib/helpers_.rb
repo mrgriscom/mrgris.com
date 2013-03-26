8ce3e925f385707bc8c1a0d5c8bc2c0e5a6e1b19
Drew Roos <droos@dimagi.com>
2013-03-25 22:18:32 -0400

    geolink helper

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -4,3 +4,8 @@ include CountryStatsHelper
 include MP3Info
 include Footnote
 include GPGInfo
+
+def geolink(lat, lon, span, name=nil)
+  name ||= 'loc:%s,%s' % [lat, lon]
+  'http://maps.google.com/?q=%{name}&ll=%{lat},%{lon}&spn=%{span},%{span}&t=h' % {:name => name, :lat => lat, :lon => lon, :span => span}
+end
