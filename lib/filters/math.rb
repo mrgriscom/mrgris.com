c97410719d7570c824b3e512324a09bc5e2e3abe
Drew Roos <mrgriscom@gmail.com>
2013-12-25 16:27:35 +0200

    tide post draft; tex support

---
+++ lib/filters/math.rb
@@ -0,0 +1,20 @@
+module Nanoc3::Filters
+  class MathTex < Nanoc3::Filter
+    identifiers :mathtex
+    
+    def run(content, params={})
+      image_dir = "output/img/math/"
+      ext = "png"
+      if (!File.exists?(image_dir))
+        system("mkdir -p " + image_dir)
+      end
+      content = content.gsub(/\$\$(.+?)\$\$/) { |m|
+        command = "texvc /tmp " + image_dir + " '" + m[2..-3] + "' utf8"
+        digest = `#{command}`.split(/\n/)[0][1..-1]
+        "<span class='equation'><img src='/img/math/" + digest + "." + ext + "' /></span>"
+      }
+      content
+    end
+  end
+end
+
