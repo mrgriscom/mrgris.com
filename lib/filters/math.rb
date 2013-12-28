962153e37004c0be2937d776bd11726f2f24e475
Drew Roos <mrgriscom@gmail.com>
2013-12-28 22:14:14 +0200

    final draft and numeric formatting

--- lib/filters/math.rb
+++ lib/filters/math.rb
@@ -8,15 +8,36 @@ module Nanoc3::Filters
       if (!File.exists?(image_dir))
         system("mkdir -p " + image_dir)
       end
+      # process latex
       content = content.gsub(/\$\$(.+?)\$\$/) { |m|
         command = "texvc /tmp " + image_dir + " '" + m[2..-3] + "' utf8"
         digest = `#{command}`.split(/\n/)[0][1..-1]
         "<span class='equation'><img src='/img/math/" + digest + "." + ext + "' /></span>"
       }
+      # process numerical quantities
       content = content.gsub(/##(.+?)##/) { |m|
         quant = m[2..-3]
+        data = quant.match(/(?<num>[-+]?\d*\.?\d*)([Ee](?<exp>[-+]?\d+))?(?<suffix>.*)/)
+        num = data['num']
+        exp = data['exp']
+        suffix = data['suffix']
 
-        "<span class='quantity'>" + quant + "</span>"
+        num = num.gsub(/-/) { |m| '&minus;' }
+        num = num.sub(/^[^\d.]*\d+/) { |m| m.sub(/\d+/) { |m| 
+          m.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse  # add commas
+        }}
+        s = num
+
+        if exp
+          exp = exp.gsub(/-/) { |m| '&minus;' }
+          s += '&times;10<sup>' + exp + '</sup>'
+        end
+
+        suffix = suffix.gsub('*', '&middot;')
+        suffix = suffix.gsub(/\^\w+/) { |m| '<sup>' + m[1..-1] + '</sup>' }
+        s += suffix
+
+        "<span class='quantity'>" + s + "</span>"
       }
       content
     end
